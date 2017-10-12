b77bbf3dbe2e6371ace352ee5277f6b950312aa9
theinternetftw <theinternetftw@gmail.com>
2017-10-12 19:50:29 -0400

    Slightly smaller sign conversion

--- xyppy/zmath.py
+++ xyppy/zmath.py
@@ -1,10 +1,10 @@
 def to_signed_word(word):
     if word & 0x8000:
-        return -((~word & 0xffff)+1)
+        return (word & 0x7fff) - 0x8000
     return word
 
 def to_signed_char(char):
     if char & 0x80:
-        return -((~char & 0xff)+1)
+        return (char & 0x7f) - 0x80
     return char
 
